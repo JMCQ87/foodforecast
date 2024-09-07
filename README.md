094dde10c22176065b754f203b26bbacbae63812
Johann Quassowski <qmtutor@gmail.com>
2024-09-07 15:42:09 +0200

    Initial table structure

---
+++ README.md
@@ -0,0 +1,10 @@
+Team AFP ftw!
+
+This sets up the db structure for tracking consumption, waste and headcount over time, per week.
+
+
+> $ flask db init
+
+> flask shell
+
+> sqlite3 canteen.db
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///canteen.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Consumption(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Time
+    year = db.Column(db.Integer)
+    week = db.Column(db.Integer)
+
+    # MEALS
+    meat = db.Column(db.Integer)
+    veggie = db.Column(db.Integer)
+    vegan = db.Column(db.Integer)
+    
+    headcount = db.Column(db.Integer) # total number of employees that could have gotten a meal
+    waste = db.Column(db.Integer) # the leftover trash
