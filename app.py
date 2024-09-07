e43149ae510a0f74b3fd322d3065d3aa98840c75
sanath95 <sanath.vh@gmail.com>
2024-09-08 01:46:39 +0200

    /gather data

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
 import random
+import numpy as np
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///canteen.db'
 
@@ -30,6 +32,25 @@ class Consumption(db.Model):
     
     headcount = db.Column(db.Integer) # total number of employees that could have gotten a meal
     waste = db.Column(db.Integer) # the leftover trash
+
+@app.route('/gather')
+def gather_data():
+    def add_data(year, w, m, veg, vegan, h, waste):
+        c = Consumption(year=year, week=w, meat=m, veggie=veg, vegan=vegan, headcount=h, waste=waste)
+        db.session.add(c)
+        db.session.commit()
+    for year in range(2020, 2025):
+        x1 = np.linspace(0, np.pi*3, 52)
+        n = np.random.normal(scale=5, size=len(x1))
+        meats = 800 + (100 * np.sin(x1) + n)
+        vegans = 800 + (50 * np.sin(x1) + n)
+        vegs = 800 + (25 * np.sin(x1) + n)
+        headcount = meats+vegans+vegs
+        weeks = range(52)
+        for w, m, veg, vegan, h in zip(weeks, meats, vegs, vegans, headcount):
+            add_data(int(year), int(w), int(m), int(veg), int(vegan), int(h), np.random.randint(3, 8))
+
+    return "data added!"
 
 @app.route('/plot')
 def plot_png():
